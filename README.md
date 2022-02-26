e299f5c4f04f691cd711700b92fa5a1410f03e55
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-02-26 20:42:10 +0100

    Add common credential client
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Install the package :
 
 ```bash
-go get github.com/Cleopha/ifttt-like-common@v1.0.5
+go get github.com/Cleopha/ifttt-like-common@v1.0.6
 ```
---
+++ credentials/credentials.go
@@ -0,0 +1,45 @@
+package credentials
+
+import (
+	"context"
+	"fmt"
+	"github.com/Cleopha/ifttt-like-common/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn *grpc.ClientConn
+	clt  protos.CredentialServiceClient
+}
+
+func NewCredentialClient(port string) (*Client, error) {
+	conn, err := grpc.Dial(":"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial: %w", err)
+	}
+
+	conn.Connect()
+	clt := protos.NewCredentialServiceClient(conn)
+
+	return &Client{
+		conn,
+		clt,
+	}, nil
+}
+
+func (c *Client) InsertCredential(ctx context.Context, in *protos.InsertCredentialRequest, opts ...grpc.CallOption) (*protos.Credential, error) {
+	return c.clt.InsertCredential(ctx, in, opts...)
+}
+
+func (c *Client) GetCredential(ctx context.Context, in *protos.GetCredentialRequest, opts ...grpc.CallOption) (*protos.Credential, error) {
+	return c.clt.GetCredential(ctx, in, opts...)
+}
+
+func (c *Client) UpdateCredential(ctx context.Context, in *protos.UpdateCredentialRequest, opts ...grpc.CallOption) (*protos.Credential, error) {
+	return c.clt.UpdateCredential(ctx, in, opts...)
+}
+
+func (c *Client) RemoveCredential(ctx context.Context, in *protos.RemoveCredentialRequest, opts ...grpc.CallOption) (*protos.Credential, error) {
+	return c.clt.RemoveCredential(ctx, in, opts...)
+}
